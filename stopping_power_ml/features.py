9585ba9978f4c97de93dacf9c75a7efa23a4b7d5
SirK8N <135128247+SirK8N@users.noreply.github.com>
2025-06-06 14:20:33 -0500

    Update features.py
    Altered conversion in IonIonForce.featurize() to 
    return correct units eV/Angstrom to Hartree/Bohr

--- stopping_power_ml/features.py
+++ stopping_power_ml/features.py
@@ -115,7 +115,9 @@ class IonIonForce(ProjectileFeaturizer):
 
         # Compute force acting against the direction of particle travel
         my_force = ewald.forces[-1, :]
-        return [-1 * np.dot(my_force, velocity) / np.linalg.norm(velocity) * 0.03674932]
+
+        # Convert force from eV/Angstrom to 1 Hartree/Bohr and return result
+        return [my_force * 0.01944688972] # 1 / 51.421
 
     def implementors(self):
         return ['Logan Ward']
